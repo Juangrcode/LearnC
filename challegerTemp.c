6a264f32c932ed751213e21def13088dd4297d3d
Sandwich7u7 <johan31156stivenramirez@gmail.com>
2020-12-13 08:08:18 -0600

    modified:   challenger.c

--- challegerTemp.c
+++ challegerTemp.c
@@ -2,3 +2,6 @@
 // 2. Realiza la operacion para convertir de grados Fahrenheit a Celcius
 // 3. imprime el resultado de la conversion
 // formula --Celcius = (fahrenheit - 32) * 5 / 9----
+#include <stdlib.h>
+#include <stdio.h>
+
