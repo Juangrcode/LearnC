38cb7e3ade3d484177cad81db27535a98ccc793f
Sandwich7u7 <johan31156stivenramirez@gmail.com>
2020-12-15 19:28:11 -0600

    Intento de envio al repositorio: challenger completado

--- challegerTemp.c
+++ challegerTemp.c
@@ -5,3 +5,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int main () {
+
+int GF ; 
+
+
+printf ("Cual es el grado que quiere poner? \n");
+scanf ("%i",&GF);
+
+float res = ((GF - 32) * 5/9);
+printf ("El resultado de pasar grados Fahrenheit a Celcius es: %f \n ", res);
+
+
+}
+
+
+
+
+
+}
