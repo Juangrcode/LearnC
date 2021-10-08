f45c4fe5cbd0cbbf28baccaeb5419e8e985f4a13
JohanSRV <johan31156stivenramirez@gmail.com>
2021-10-08 15:25:30 -0500

    [ADD] Python

---
+++ POO/CursoPOOUber/python/main.py
@@ -0,0 +1,8 @@
+from POO.CursoPOOUber.python.account import Account
+from car import Car
+
+if __name__ == "__main__":
+    print("Hola Mundo")
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876") )
+    print(vars(car))
+    print(vars(car.driver))
